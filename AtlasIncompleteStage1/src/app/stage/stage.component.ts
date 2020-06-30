dd9e070375a8cfdfe84e255b8925dea21a6579f9
Kaleb Bausch <kbausch@mtech.edu>
2020-06-29 22:32:37 -0600

    animations working by not "this.stage = result"

--- AtlasIncompleteStage1/src/app/stage/stage.component.ts
+++ AtlasIncompleteStage1/src/app/stage/stage.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
-import { map } from 'rxjs/operators';
 import { HostListener } from '@angular/core';
 
 import {
@@ -18,25 +17,38 @@ import {
   animations: [
     trigger('position', [
       state('0', style({
-        transform: 'translateX(0%)'
+        transform: 'translateX(-25%)'
       })),
       state('1', style({
-        transform: 'translateX(25%)'
+        transform: 'translateX(0%)'
       })),
       state('2', style({
-        transform: 'translateX(50%)'
+        transform: 'translateX(17%)'
       })),
       state('3', style({
-        transform: 'translateX(75%)'
+        transform: 'translateX(34%)'
       })),
       state('4', style({
-        transform: 'translateX(100%)'
+        transform: 'translateX(51%)'
       })),
       state('5', style({
-        transform: 'translateX(125%)'
+        transform: 'translateX(68%)'
       })),
       state('6', style({
-        transform: 'translateX(150%)'
+        transform: 'translateX(85%)'
+      })),
+      state('7', style({
+        transform: 'translateX(105%)'
+      })),
+      transition(":increment", animate('500ms ease')),
+      transition(":decrement", animate('500ms ease'))
+    ]),
+    trigger('direction', [
+      state('ArrowRight', style({
+        transform: 'rotateY(0deg)'
+      })),
+      state('ArrowLeft', style({
+        transform: 'rotateY(180deg)'
       }))
     ])
   ]
@@ -45,30 +57,27 @@ export class StageComponent implements OnInit {
 
   characterList: object;
   stage: object;
+  lastmove = 'ArrowRight';
   @HostListener('document:keyup', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
     this.getInput(event.key);
   };
 
   constructor(private db: AngularFireDatabase) {
-    db.list('/characters').snapshotChanges().pipe(
-      map(actions => {
-        return actions.map(a => {
-          return a.payload.val();
-        });
-      })).subscribe(result => this.characterList = result);
-    db.list('/stage').snapshotChanges().pipe(
-      map(actions => {
-        return actions.map(a => {
-          return {
-            key: a.key,
-            content: a.payload.val()
-          };
-        });
-      })).subscribe(result => {
-        this.stage = result;
-        console.log(this.stage);
-      });
+    db.list('/characters').valueChanges().subscribe(result => this.characterList = result);
+    db.list('/stage').valueChanges().subscribe((result: any) => {
+      // The reason that I have to do this unideal for-loop is because if I simply "this.stage = result" everytime
+      // then the *ngFor loop will destroy and re-render each character, ruining the animations.
+      for (let i in result) {
+        if (this.stage === undefined) {
+          this.stage = result;
+        } else {
+          this.stage[i].position = result[i].position;
+          this.stage[i].direction = result[i].direction;
+          this.stage[i].expression = result[i].expression;
+        }
+      }
+    });
   }
 
   ngOnInit(): void {
@@ -77,6 +86,7 @@ export class StageComponent implements OnInit {
   addSteve() {
     const updates = {
       'stage/steve': {
+        direction: this.lastmove,
         position: '1',
         expression: 'default'
       }
@@ -85,99 +95,73 @@ export class StageComponent implements OnInit {
   }
 
   getInput(key: string) {
-    console.log(key);
+    //console.log(key);
     const updates = {};
+    updates['stage/steve'] = {
+      direction: this.lastmove,
+      position: this.stage[0].position,
+      expression: this.stage[0].expression
+    };
     if (key === 'ArrowRight') {
-      if (this.stage[0].content.position > 5) {
-        updates['stage/steve'] = {
-          position: 0,
-          expression: this.stage[0].content.expression
-        };
+      if (this.lastmove === 'ArrowRight') {
+        if (this.stage[0].position > 6) {
+          updates['stage/steve'].position = 0;
+        } else {
+          updates['stage/steve'].position++;
+        }
       } else {
-        updates['stage/steve'] = {
-          position: this.stage[0].content.position + 1,
-          expression: this.stage[0].content.expression
-        };
+        this.lastmove = 'ArrowRight';
+        updates['stage/steve'].direction = this.lastmove;
       }
     } else if (key === 'ArrowLeft') {
-      if (this.stage[0].content.position < 1) {
-        updates['stage/steve'] = {
-          position: 6,
-          expression: this.stage[0].content.expression
-        };
+      if (this.lastmove === 'ArrowLeft') {
+        if (this.stage[0].position < 1) {
+          updates['stage/steve'].position = 7;
+        } else {
+          updates['stage/steve'].position--;
+        }
       } else {
-        updates['stage/steve'] = {
-          position: this.stage[0].content.position - 1,
-          expression: this.stage[0].content.expression
-        };
+        this.lastmove = 'ArrowLeft';
+        updates['stage/steve'].direction = this.lastmove;
       }
     }
     else if (key === 'q') {
-      updates['stage/steve'] = {
-        position: this.stage[0].content.position,
-        expression: 'angry'
-      };
+      updates['stage/steve'].expression = 'angry';
     }
     else if (key === 'w') {
-      updates['stage/steve'] = {
-        position: this.stage[0].content.position,
-        expression: 'concern'
-      };
+      updates['stage/steve'].expression = 'concern';
     }
     else if (key === 'e') {
-      updates['stage/steve'] = {
-        position: this.stage[0].content.position,
-        expression: 'confused'
-      };
+      updates['stage/steve'].expression = 'confused';
     }
     else if (key === 'r') {
-      updates['stage/steve'] = {
-        position: this.stage[0].content.position,
-        expression: 'default'
-      };
+      updates['stage/steve'].expression = 'default';
     }
     else if (key === 't') {
-      updates['stage/steve'] = {
-        position: this.stage[0].content.position,
-        expression: 'furious'
-      };
+      updates['stage/steve'].expression = 'furious';
     }
     else if (key === 'y') {
-      updates['stage/steve'] = {
-        position: this.stage[0].content.position,
-        expression: 'happy'
-      };
+      updates['stage/steve'].expression = 'happy';
     }
     else if (key === 'u') {
-      updates['stage/steve'] = {
-        position: this.stage[0].content.position,
-        expression: 'kiss'
-      };
+      updates['stage/steve'].expression = 'kiss';
     }
     else if (key === 'i') {
-      updates['stage/steve'] = {
-        position: this.stage[0].content.position,
-        expression: 'oh'
-      };
+      updates['stage/steve'].expression = 'oh';
     }
     else if (key === 'o') {
-      updates['stage/steve'] = {
-        position: this.stage[0].content.position,
-        expression: 'question'
-      };
+      updates['stage/steve'].expression = 'question';
     }
     else if (key === 'p') {
-      updates['stage/steve'] = {
-        position: this.stage[0].content.position,
-        expression: 'sad'
-      };
+      updates['stage/steve'].expression = 'sad';
     }
     else if (key === '[') {
-      updates['stage/steve'] = {
-        position: this.stage[0].content.position,
-        expression: 'wink'
-      };
+      updates['stage/steve'].expression = 'wink';
     }
     this.db.database.ref().update(updates);
+  }
+
+  logAnimation($event) {
+    console.log($event)
   }
 }
