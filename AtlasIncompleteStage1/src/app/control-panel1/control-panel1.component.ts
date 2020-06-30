90eee7beeda85248553e22115ab2d81f77de4529
Kaleb Bausch <kbausch@mtech.edu>
2020-06-29 23:59:58 -0600

    quick setup before bed

--- AtlasIncompleteStage1/src/app/control-panel1/control-panel1.component.ts
+++ AtlasIncompleteStage1/src/app/control-panel1/control-panel1.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-control-panel1',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ControlPanel1Component implements OnInit {
 
-  constructor() { }
+  characterList: object;
+
+  constructor(private db: AngularFireDatabase) {
+    db.list('/characters').snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          return {
+            key: a.key,
+            content: a.payload.val()
+          };
+        });
+      })).subscribe(result => { this.characterList = result; console.log(this.characterList); });
+  }
 
   ngOnInit(): void {
   }
